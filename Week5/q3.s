dfb93632783dbd058cb42b2962c4becb36186626
apex <apex@gmail.com>
2021-02-25 15:21:24 +0530

    final

--- Week5/q3.s
+++ Week5/q3.s
@@ -2,15 +2,15 @@
 	LDR R1,=A
 	LDR R2,=B
 	
-	LOOP: LDRB R0,[R1],#1
+	loop: LDRB R0,[R1],#1
 		  CMP R0,#0
 		  BEQ END
 		  STRB R0,[R2],#1
-		  B LOOP
+		  B loop
 		  
 	END: STRB R0,[R2] 
 		 SWI 0x011
 
 .data
-	A: .ASCIZ "Hello World"
+	A: .ASCIZ "nitish"
 	B: .ASCIZ ""
