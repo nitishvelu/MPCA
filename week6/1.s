4fd93115402de016114df4e178d2da473ed63fc1
apex <apex@gmail.com>
2021-03-04 15:42:37 +0530

    done

--- week6/1.s
+++ week6/1.s
@@ -1,14 +1,14 @@
 .text
     MOV R0, #0
-    loop:
+    LOOP:
         SWI 0x201
         ADD R0,R0,#1
         MOV R4, #8000
-        delay:
+        DELAY:
             SUB R4,R4,#1
             CMP R4,#0
-            BNE delay
-        CMP R0,#4
+            BNE DELAY
+       	    CMP R0,#4
 
-        BLE loop
+        BLE LOOP
         swi 0x011
